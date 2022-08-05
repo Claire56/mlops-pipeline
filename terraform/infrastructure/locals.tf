9f83f35697f257a7895661d9f6b4ce9c2d374336
Claire Kim <kclaire56@yahoo.com>
2022-08-05 13:13:33 -0700

    final data path added

--- terraform/infrastructure/locals.tf
+++ terraform/infrastructure/locals.tf
@@ -2,7 +2,7 @@
 locals {
   s3_bucket_input_training_path = "${var.project_name}-training-data-${data.aws_caller_identity.current.account_id}"
   s3_bucket_output_models_path = "${var.project_name}-output-models-${data.aws_caller_identity.current.account_id}"
-  s3_object_training_data = "../../data/iris.csv"
+  s3_object_training_data = "../../data/final_data.csv"
   input_training_path = "s3://${var.project_name}-training-data-${data.aws_caller_identity.current.account_id}"
   output_models_path = "s3://${var.project_name}-output-models-${data.aws_caller_identity.current.account_id}"
   lambda_function_name = "config-${var.project_name}"
